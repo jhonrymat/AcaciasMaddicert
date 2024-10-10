4a396198a16efc8a214088b6bd988cab84d8a5ec
anfecaba2 <andres@nomaddi.com>
2024-10-10 17:31:05 -0500

    centre el logo del desboar del usuario

--- resources/views/components/welcome.blade.php
+++ resources/views/components/welcome.blade.php
@@ -1,7 +1,7 @@
 <div class="p-6 lg:p-8 bg-white border-b border-gray-200">
-    <x-application-logo class="" />
+    <x-application-logo class="" />     {{--las clases aca no me quieren funcionar --}}
 
-     <h1 class="mt-8 text-2xl font-medium text-gray-900">  {{--quiero centrar el logo --}}
+    <h1 class="mt-8 text-2xl font-medium text-gray-900">  
         Bienvenido a Maddicert!
     </h1>
 
